4c9d5f3e81ccd20536b1a01b2ffe69413f5fec15
enggar <ejihandoko@gmail.com>
2025-01-22 15:03:17 +0700

    implement history layout and logic

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -1,3 +1,40 @@
+import {
+	calculateCurrentCaffeineLevel,
+	coffeeConsumptionHistory,
+	getCaffeineAmount,
+	timeSinceConsumption,
+} from "../utils";
+
 export default function History() {
-	return <></>;
+	return (
+		<>
+			<div className="section-header">
+				<i className="fa-solid fa-timeline"></i>
+				<h2>History</h2>
+			</div>
+			<p>
+				<i>Hover for more information!</i>
+			</p>
+			<div className="coffee-history">
+				{Object.keys(coffeeConsumptionHistory)
+					.sort((a, b) => b - a)
+					.map((utcTime) => {
+						const coffee = coffeeConsumptionHistory[utcTime];
+						const timeSinceConsume = timeSinceConsumption(utcTime);
+						const originalAmout = getCaffeineAmount(coffee.name);
+						const remainingAmount = calculateCurrentCaffeineLevel({
+							[utcTime]: coffee,
+						});
+
+						const summary = `${coffee.name} | ${timeSinceConsume} | $${coffee.cost} | ${remainingAmount}mg / ${originalAmout}mg`;
+
+						return (
+							<div title={summary} key={utcTime}>
+								<i className="fa-solid fa-mug-hot"></i>
+							</div>
+						);
+					})}
+			</div>
+		</>
+	);
 }
